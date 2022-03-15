b0ffee432ec715cb9ba4da1aad76b269a87db081
studiosyndicatcaius <jordan.kollins@gmail.com>
2022-03-15 16:33:06 -0500

    RemovedStoryModule

--- OmegaGameFramework/Source/OmegaStory/Private/Speaker/OmegaSpeakerInterface.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Speaker/OmegaSpeakerInterface.h"
-
-
-// Add default functionality here for any IOmegaSpeakerInterface functions that are not pure virtual.
