1664565e60fb3c499a7e63a38bc968c85e5313c3
li5414 <admin@unigamestudio.com>
2022-06-17 14:18:06 +0800

    fix warning : Referenced directory 'XXX\OmegaData\Public' does not exist.

--- OmegaGameFramework/Source/OmegaFlow/OmegaFlow.build.cs
+++ OmegaGameFramework/Source/OmegaFlow/OmegaFlow.build.cs
@@ -1,5 +1,6 @@
 using UnrealBuildTool;
- 
+using System.IO;
+
 public class OmegaFlow : ModuleRules
 {
 	public OmegaFlow(ReadOnlyTargetRules Target) : base(Target)
@@ -8,7 +9,7 @@ public class OmegaFlow : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "GameplayTags", "OmegaGameFramework"});
  
-		PublicIncludePaths.AddRange(new string[] {"OmegaFlow/Public"});
-		PrivateIncludePaths.AddRange(new string[] {"OmegaFlow/Private"});
+		PublicIncludePaths.AddRange(new string[] {Path.Combine(ModuleDirectory,"Public")});
+		PrivateIncludePaths.AddRange(new string[] {Path.Combine(ModuleDirectory,"Private")});
 	}
 }
