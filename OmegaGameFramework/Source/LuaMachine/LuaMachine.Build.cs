59fd54fa7d988e9a114dbb46de48f8e4ffebbda3
Andrei Müller (Andy Miira) <andreiagmu@gmail.com>
2024-03-25 04:25:02 -0300

    LuaMachine.Build.cs: Revert "PrecompileForTargets = PrecompileTargetsType.Any"
    The previous commit that added the "PrecompileForTargets = PrecompileTargetsType.Any;" line was intended to fix a game build bug. But the bugfix (where Unreal suggested adding the above line) is probably a misleading bugfix. (i.e.: generic error message for certain kinds of bugs)

--- OmegaGameFramework/Source/LuaMachine/LuaMachine.Build.cs
+++ OmegaGameFramework/Source/LuaMachine/LuaMachine.Build.cs
@@ -9,8 +9,6 @@ public class LuaMachine : ModuleRules
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
         bUseUnity = false;
 
-        PrecompileForTargets = PrecompileTargetsType.Any;
-
         PublicIncludePaths.AddRange(
             new string[] {
 				// ... add public include paths required here ...
